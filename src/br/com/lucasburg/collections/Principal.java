78c168257148d0c88e543a2279758c2dd3014d08
Lucas Burg <lucasburgmota@gmail.com>
2015-12-09 13:57:13 -0200

    Iniciando estudos com collections

---
+++ src/br/com/lucasburg/collections/Principal.java
@@ -0,0 +1,40 @@
+package br.com.lucasburg.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		List alunos = new ArrayList();
+		Scanner leitor = new Scanner(System.in);
+		String nome = "sair";
+		int sizeAlunos = 0;
+		
+		do {
+			
+			System.out.println("Informe o nome do aluno: ");
+			nome = leitor.nextLine();
+			
+			if (!nome.equals("sair")) {
+				alunos.add(nome);
+			}
+			
+			
+		} while(!nome.equals("sair"));
+		
+		
+		sizeAlunos = alunos.size();
+		
+		for (int i = 0; i < sizeAlunos; i++) {
+			System.out.println(alunos.get(i));
+		}
+		
+		
+		leitor.close();
+		
+
+	}
+
+}
