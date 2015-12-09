145b5f97e77fdc323bd191516695517b1f915735
Lucas Burg <lucasburgmota@gmail.com>
2015-12-09 13:29:51 -0200

    Função arraycopy da classe System

--- src/br/com/lucasburg/arrays/Principal.java
+++ src/br/com/lucasburg/arrays/Principal.java
@@ -7,8 +7,15 @@ public class Principal {
 
 	public static void main(String[] args) {
 		final String barra = "--------------------------------------------------";
-		String[] alunos = new String[3];
+		String[] alunos  = new String[3];
 		String[] alunos2 = new String[3];
+		String[] alunos3 = new String[3];
+		int indiceMaria;
+		
+		alunos3[0] = "Mateus";
+		alunos3[1] = "Ana";
+		alunos3[2] = "Dilma";
+		
 		Scanner leitor = new Scanner(System.in);
 		
 		
@@ -24,6 +31,7 @@ public class Principal {
 		
 		Arrays.sort(alunos);
 		Arrays.sort(alunos2);
+		Arrays.sort(alunos3);
 		
 		for (int i = 0; i <= 2; i++) {
 			System.out.printf("O aluno %d se chama %s%n", i, alunos[i]);
@@ -32,6 +40,26 @@ public class Principal {
 		System.out.println(barra);
 		
 		System.out.println("Os arrays são iguais ? "+Arrays.equals(alunos, alunos2));
+		
+		System.out.println(barra);
+		
+		indiceMaria = Arrays.binarySearch(alunos, "Maria");
+		
+		System.out.println("Procurar por Maria... seu indice é: " + indiceMaria);
+		
+		if (indiceMaria >= 0) {
+			System.out.println(barra);
+			System.out.println("Copiar Maria...");
+			System.arraycopy(alunos, indiceMaria, alunos3, 0, 1);
+		}
+		
+		System.out.println(barra);
+		
+		for (int i = 0; i <= 2; i++) {
+			System.out.printf("O aluno %d se chama %s / O aluno3 %d se chama %s %n", i, alunos[i], i, alunos3[i]);
+		}
+		
+		
 		
 		leitor.close();
 
