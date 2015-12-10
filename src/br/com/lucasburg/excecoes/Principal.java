67366c5bffb6fd88dbcb3643e03037f60171debd
Lucas Burg <lucasburgmota@gmail.com>
2015-12-10 17:02:40 -0200

    Estudando varios catchs e exceptions

--- src/br/com/lucasburg/excecoes/Principal.java
+++ src/br/com/lucasburg/excecoes/Principal.java
@@ -1,31 +1,61 @@
 package br.com.lucasburg.excecoes;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		int a = 1;
 		int b = 2;
-		int c = 0;
 		double result = 0;
 		
+		Scanner leitor = new Scanner(System.in);
 		
+		System.out.println("Informe dois numeros para divisão.");
 		
 		try {
-			result = dividir(b, c);
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
+			
+			a = lerNumero(leitor, "Número 1: ");
+			
+			b = lerNumero(leitor, "Número 2: ");
+			
+			result = dividir(a, b);
+		
+		} catch (ArithmeticException ex) {
+		
+			System.out.println(ex.toString());
+		
+		} catch (InputMismatchException ex) {
+			
+			System.out.println(ex.toString());
+			
+		} catch (Exception ex) {
+			
+			System.out.println(ex.toString());
+		
+		} finally {
+		
+			System.out.println("Ótimo terminamos a exception.");
+		
 		}
 		
 		
 		
-		System.out.print(result);
+		System.out.println("Resultado: " + result);
 				
-
+		leitor.close();
+		
 	}
 	
 	
-	static int dividir(int value1, int value2) throws Exception {
+	private static int dividir(int value1, int value2) {
 		return value1 / value2;
+	}
+	
+	private static int lerNumero(Scanner leitor, String label) {
+		System.out.println(label);
+		return leitor.nextInt();
 	}
 
 }
