202b52ac5f3d0ba055af8c8eb9873d936879c41a
Lucas Burg <lucasburgmota@gmail.com>
2015-12-11 14:43:02 -0200

    Estudo de atributos estaticos

--- src/br/com/lucasburg/application/Principal.java
+++ src/br/com/lucasburg/application/Principal.java
@@ -1,10 +1,14 @@
 package br.com.lucasburg.application;
 
 import br.com.lucasburg.oo.Animal;
+import br.com.lucasburg.statics.Empresa;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		/**
+		 * Utilizado para o pacote oo
+		 */
 		try {
 			Animal gato = new Animal();
 			gato.setEspecie("Gato");
@@ -16,6 +20,21 @@ public class Principal {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
+		Empresa empresa1 = new Empresa();
+		
+		empresa1.nome = "Buum Digital";
+		empresa1.ano  = 2013;
+
+		System.out.println("Nome da empresa: " + empresa1.nome + " Ano: " +empresa1.ano + " Ativa: " + Empresa.ativa);
+		
+		Empresa empresa2 = new Empresa();
+		
+		empresa2.nome = "Circulos";
+		empresa2.ano  = 2015;
+		
+		System.out.println("Nome da empresa: " + empresa2.nome + " Ano: " +empresa2.ano + " Ativa: " + Empresa.ativa);
+
 		
 	}
 
---
+++ src/br/com/lucasburg/statics/Empresa.java
@@ -0,0 +1,8 @@
+package br.com.lucasburg.statics;
+
+public class Empresa {
+	public String nome;
+	public int ano;
+	public String cnpj;
+	public static boolean ativa;
+}
