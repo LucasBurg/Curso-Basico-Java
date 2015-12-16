da2c1aedd9778460f9d9f3c4f1771a0e5955c321
Lucas Burg <lucasburgmota@gmail.com>
2015-12-16 17:22:14 -0200

    Removido import desnesessario

--- src/br/com/lucasburg/application/Principal.java
+++ src/br/com/lucasburg/application/Principal.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import javax.sql.ConnectionEvent;
-
 import br.com.lucasburg.classes.PrincipaisClasses;
 import br.com.lucasburg.conexao.Conexao;
 import br.com.lucasburg.database.FabricaConexao;
